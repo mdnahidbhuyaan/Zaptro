5d2a6cca639230177e37cca826e9e9fc09864ba2
mdnahidbhuyaan <mdnahidbuiyan14@gmail.com>
2025-11-25 21:13:41 +0600

    commerce

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,33 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
+import { MapPinIcon } from 'lucide-react'
+import { FaCaretDown } from 'react-icons/fa'
 
 const Navbar = () => {
+  const location = false
   return (
     <div className='bg-white py-3 shadow-2xl'>
       <div className='max-w-6xl mx-auto flex justify-between items-center'>
         {/* logo section */}
-        <div className=''>
+        <div className='flex gap-7 items-center'>
           <Link to='/'><h1 className='text-3xl font-bold'> <span className='text-red-600 font-serif'>Z</span>aptro</h1></Link>
+          <div className='flex gap-1 items-center cursor-pointer text-gray-700'>
+               <MapPinIcon className='text-red-500'/>
+               <span className='font-semibold'>{location ? <div></div>:"Add Address"}</span>
+               <FaCaretDown />
+          </div>
         </div>
-
+        {/* menu section */}
+        <nav>
+           <ul className='flex gap-7 items-center text-xl font-semibold'>
+            <NavLink to={"/"} className={({isActive})=>`${isActive ? "border-b-3 transition-all border-red-500 rounded-sm" :"text-black"}cursor-pointer`}><li>Home </li></NavLink>
+            <NavLink to={"/product"} className={({isActive})=>`${isActive ? "border-b-3 transition-all border-red-500 rounded-sm" :"text-black"}cursor-pointer`}><li>Product </li></NavLink>
+            <NavLink to={"/about"} className={({isActive})=>`${isActive ? "border-b-3 transition-all border-red-500 rounded-sm" :"text-black"}cursor-pointer`}><li>About </li></NavLink>
+            <NavLink to={"/contact"} className={({isActive})=>`${isActive ? "border-b-3 transition-all border-red-500 rounded-sm" :"text-black"}cursor-pointer`}><li>Contact</li></NavLink>
+            
+          
+           </ul>
+        </nav>
       </div>
     </div>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div className='max-w-6xl bg-white items-center mx-auto '>
-     <h1 className='text-5xl mt-20 font-semibold'> -<span className='text-red-600'>One</span> day your hardwork define.</h1>
-     <h2 className='text-5xl font-semibold mt-5'><span className='text-red-600'>Who</span> are you...!</h2>
+
     </div>
   )
 }
