dc15a4c03d8cf70a040ca19bc7ccb1e193f675f9
mdnahidbhuyaan <mdnahidbuiyan14@gmail.com>
2025-11-26 21:42:20 +0600

    hello

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,36 +1,102 @@
-import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
-import { MapPinIcon } from 'lucide-react'
-import { FaCaretDown } from 'react-icons/fa'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { MapPinIcon } from "lucide-react";
+import { FaCaretDown } from "react-icons/fa";
+import { IoCartOutline } from "react-icons/io5";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
 
 const Navbar = () => {
-  const location = false
+  const location = false;
   return (
-    <div className='bg-white py-3 shadow-2xl'>
-      <div className='max-w-6xl mx-auto flex justify-between items-center'>
+    <div className="bg-white py-3 shadow-2xl">
+      <div className="max-w-6xl mx-auto flex justify-between items-center">
         {/* logo section */}
-        <div className='flex gap-7 items-center'>
-          <Link to='/'><h1 className='text-3xl font-bold'> <span className='text-red-600 font-serif'>Z</span>aptro</h1></Link>
-          <div className='flex gap-1 items-center cursor-pointer text-gray-700'>
-               <MapPinIcon className='text-red-500'/>
-               <span className='font-semibold'>{location ? <div></div>:"Add Address"}</span>
-               <FaCaretDown />
+        <div className="flex gap-7 items-center">
+          <Link to="/">
+            <h1 className="text-3xl font-bold">
+              {" "}
+              <span className="text-red-600 font-serif">Z</span>aptro
+            </h1>
+          </Link>
+          <div className="flex gap-1 items-center cursor-pointer text-gray-700">
+            <MapPinIcon className="text-red-500" />
+            <span className="font-semibold">
+              {location ? <div></div> : "Add Address"}
+            </span>
+            <FaCaretDown />
           </div>
         </div>
         {/* menu section */}
-        <nav>
-           <ul className='flex gap-7 items-center text-xl font-semibold'>
-            <NavLink to={"/"} className={({isActive})=>`${isActive ? "border-b-3 transition-all border-red-500 rounded-sm" :"text-black"}cursor-pointer`}><li>Home </li></NavLink>
-            <NavLink to={"/product"} className={({isActive})=>`${isActive ? "border-b-3 transition-all border-red-500 rounded-sm" :"text-black"}cursor-pointer`}><li>Product </li></NavLink>
-            <NavLink to={"/about"} className={({isActive})=>`${isActive ? "border-b-3 transition-all border-red-500 rounded-sm" :"text-black"}cursor-pointer`}><li>About </li></NavLink>
-            <NavLink to={"/contact"} className={({isActive})=>`${isActive ? "border-b-3 transition-all border-red-500 rounded-sm" :"text-black"}cursor-pointer`}><li>Contact</li></NavLink>
-            
-          
-           </ul>
+
+        <nav className="flex gap-7 items-center">
+          <ul className="flex gap-7 items-center text-xl font-semibold">
+            <NavLink
+              to={"/"}
+              className={({ isActive }) =>
+                `${
+                  isActive
+                    ? "border-b-3 transition-all border-red-500 rounded-sm"
+                    : "text-black"
+                }cursor-pointer`
+              }
+            >
+              <li>Home </li>
+            </NavLink>
+            <NavLink
+              to={"/product"}
+              className={({ isActive }) =>
+                `${
+                  isActive
+                    ? "border-b-3 transition-all border-red-500 rounded-sm"
+                    : "text-black"
+                }cursor-pointer`
+              }
+            >
+              <li>Product </li>
+            </NavLink>
+            <NavLink
+              to={"/about"}
+              className={({ isActive }) =>
+                `${
+                  isActive
+                    ? "border-b-3 transition-all border-red-500 rounded-sm"
+                    : "text-black"
+                }cursor-pointer`
+              }
+            >
+              <li>About </li>
+            </NavLink>
+            <NavLink
+              to={"/contact"}
+              className={({ isActive }) =>
+                `${
+                  isActive
+                    ? "border-b-3 transition-all border-red-500 rounded-sm"
+                    : "text-black"
+                }cursor-pointer`
+              }
+            >
+              <li>Contact</li>
+            </NavLink>
+          </ul>
+          <Link to={"/cart"} className="relative">
+            <IoCartOutline className="h-7 w-7 " />
+            <span className="bg-red-500 px-2 rounded-full absolute -top-3 -right-3 text-white">
+              0
+            </span>
+          </Link>
+          <div>
+            <SignedOut>
+              <SignInButton className="bg-red-500 text-white py-1 px-5 rounded-full cursor-pointer" />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </div>
         </nav>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
